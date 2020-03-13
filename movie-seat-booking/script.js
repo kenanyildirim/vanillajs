e223771a4fae96908322f34bce430f15f8c125a6
kenanyildirim <kyildirim07@gmail.com>
2020-03-13 13:20:28 +0300

    localStorage operations added

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -5,6 +5,8 @@ const count=document.getElementById('count');
 const total=document.getElementById('total');
 const movieSelect=document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice=+movieSelect.value;
 
 
@@ -29,6 +31,25 @@ total.innerText=selectedSeatsCount*ticketPrice;
 
 }
 
+//Det date from localstorage and populate UI
+
+function populateUI(){
+    const selectedSeats= JSON.parse(localStorage.getItem('selectedSeats'));
+    if(selectedSeats!==null && selectedSeats.length>0){
+        seats.forEach((seat,index)=>{
+            if(selectedSeats.indexOf(index)>-1){
+                seat.classList.add('selected');
+            }
+        });
+    }
+    const selectedMovieIndex=localStorage.getItem('selectedMovieIndex');
+    if(selectedMovieIndex!==null){
+        movieSelect.selectedIndex=selectedMovieIndex;
+    }
+    
+    console.log(selectedSeats)
+}
+
 //movie change event
 
 movieSelect.addEventListener('change',(e)=>{
@@ -46,5 +67,7 @@ container.addEventListener('click',(e)=>{
 
        updateSelectedCount();
     }
-})
+});
+
+updateSelectedCount();
 
